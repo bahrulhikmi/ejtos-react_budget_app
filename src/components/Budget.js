39a8ae6ec6876cf3b2e4923aacaf9ae9e9e239bc
bahrul hikmi <bahrulhikmi@gmail.com>
2023-08-13 18:32:47 -0400

    hh

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,8 +3,21 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
+      
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input
+        required='required'
+        type='number'
+        id='cost'
+        value={budget}
+        style={{ marginLeft: '2rem' , size: 10}}
+        onChange={(event) => {            
+            var cost  =  parseInt(event.target.value);
+            if(cost > 20000){
+                alert('Cannot exceed £20.000')
+            }
+        }}>
+        </input></span>
         </div>
     );
 };
